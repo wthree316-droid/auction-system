44435eb4ad953f1bee48208420ee68a48589e23c
wthree316-droid <wthree316@gmail.com>
2025-11-28 05:56:54 +0700

    Fix formatting and remove unnecessary comments

--- firebase-config.js
+++ firebase-config.js
@@ -1,21 +1,19 @@
-// 1. นำเข้า Library (ใช้เวอร์ชัน 10.7.1 เหมือนกันทุกไฟล์เพื่อความเสถียร)
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
-
 const firebaseConfig = {
     apiKey: "AIzaSyCQOSvE07bNi2WfCymRdOabDewgYRs4UM4", 
-    authDomain: "auction-system-e9801.firebaseapp.com", 
-    projectId: "auction-system-e9801",                  
-    storageBucket: "auction-v2-img-999",                
-    messagingSenderId: "1089558422014",                 
-    appId: "1:1089558422014:web:4052e4b6e8f391c5a5a0af" 
+    authDomain: "auction-system-e9801.firebaseapp.com",
+    projectId: "auction-system-e9801",
+    storageBucket: "auction-v2-img-999", 
+    messagingSenderId: "1089558422014",
+    appId: "1:1089558422014:web:4052e4b6e8f391c5a5a0af"
 };
 
-// 3. เริ่มต้นระบบ (Initialize)
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
+
 
 export { app, db, auth };
