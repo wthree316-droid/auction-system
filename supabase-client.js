e20f4c71d8c08cbf2fdb301cc9036b649c617831
wthree316-droid <wthree316@gmail.com>
2025-12-08 21:03:46 +0700

    Update supabase-client.js
    Signed-off-by: wthree316-droid <wthree316@gmail.com>

--- supabase-client.js
+++ supabase-client.js
@@ -1,6 +1,6 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 
 const supabaseUrl = 'https://qmoksrovighvixenknnq.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFtb2tzcm92aWdodml4ZW5rbm5xIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2NDQzNDQ4MiwiZXhwIjoyMDgwMDEwNDgyfQ.8QMRjj0qVOFRi_phGaVAvUL-yA9PorMOzocGURbKpyQ';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFtb2tzcm92aWdodml4ZW5rbm5xIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ0MzQ0ODIsImV4cCI6MjA4MDAxMDQ4Mn0.9t_7iq3o1gplFjUolLtgaIXbWTh9B7SeO4D8Pr1YqsY'; 
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
