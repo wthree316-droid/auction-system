468fa00bef92adc39b532cc61579570d3c5d1632
wthree316-droid <wthree316@gmail.com>
2025-12-19 04:08:34 +0700

    Refactor Supabase client to use config variables
    Signed-off-by: wthree316-droid <wthree316@gmail.com>

--- supabase-client.js
+++ supabase-client.js
@@ -1,6 +1,4 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
+import { CONFIG } from './config.js';
 
-const supabaseUrl = 'https://qmoksrovighvixenknnq.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFtb2tzcm92aWdodml4ZW5rbm5xIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ0MzQ0ODIsImV4cCI6MjA4MDAxMDQ4Mn0.9t_7iq3o1gplFjUolLtgaIXbWTh9B7SeO4D8Pr1YqsY'; 
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(CONFIG.SUPABASE_URL, CONFIG.SUPABASE_KEY);
